859abd05aee057d8e05d1110d07215412d40535f
KAMO-PC\Admin <you@example.com>
2025-06-08 23:28:09 +0300

    homeObjectEx

--- ObjectEx/ObjectEx/Program.cs
+++ ObjectEx/ObjectEx/Program.cs
@@ -10,15 +10,17 @@ internal class Program
         var p2 = new Person { Name = "Alex", Age = 33 };
         var p3 = new Person { Name = "Yury", Age = 41 };
 
-
         Console.WriteLine(p1);
+        Console.WriteLine($"HashCode of p1: {p1.GetHashCode()}");
+        Console.WriteLine($"HashCode of p2: {p2.GetHashCode()}");
+        Console.WriteLine($"HashCode of p3: {p3.GetHashCode()}");
 
-        Console.WriteLine(p1.Equals(p2));
+        Console.WriteLine(p1.Equals(p2)); // false
+        Console.WriteLine(p3.Equals(p1)); // true
+        Console.WriteLine(p3 == p1); // true
 
-        Console.WriteLine(p3.Equals(p1));
-
-        Console.WriteLine(p3 == p1);
-
+        Console.WriteLine($"p1 < p2: {p1 < p2}"); // false
+        Console.WriteLine($"p1 > p2: {p1 > p2}"); // 
 
     }
 }
